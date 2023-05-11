abfa3e8b8c0cb56b7abb9ce46e6d252344bc3bf3
ClearanceObvious <95777574+ClearanceObvious@users.noreply.github.com>
2023-05-11 19:24:39 +0200

    Create nodes.py

---
+++ nodes.py
@@ -0,0 +1,143 @@
+from enum import Enum
+from dataclasses import dataclass
+from ttoken import TokenType
+
+class NodeType(Enum):
+    NewLineNode = -1
+
+    NumberNode = 0
+    StringNode = 1
+    BooleanNode = 2
+    NullNode = 3
+
+    BinOpNode = 4
+    UnOpNode = 5
+
+    EqNode = 6
+    CondNode = 7
+
+    VarCreateNode = 8
+    VarAccessNode = 9
+    VarOverrideNode = 10
+
+    ArrayNode = 11
+    ObjectNode = 12
+
+    IndexNode = 13
+    DataStructOverrideNode = 14
+
+    FunctionNode = 15
+    FunctionCallNode = 16
+    ReturnNode = 17
+
+    IfStatementNode = 18
+    ForLoopNode = 19
+    WhileLoopNode = 20
+
+    BaseGlobalLog = 21
+    BaseGlobalSleep = 22
+
+
+@dataclass
+class Node:
+    type: NodeType
+    value: any = 0
+
+    def __hash__(self):
+        return self.type.value
+
+class BinOpNode(Node):
+    def __init__(self, left: Node, op: TokenType, right: Node):
+        super().__init__(NodeType.BinOpNode, 0)
+        self.left = left
+        self.op = op
+        self.right = right
+
+class UnOpNode(Node):
+    def __init__(self, node: Node):
+        super().__init__(NodeType.UnOpNode, 0)
+        self.node = node
+
+class CondNode(Node):
+    def __init__(self, left: Node, op: Node, right: Node):
+        super().__init__(NodeType.CondNode, 0)
+        self.left = left
+        self.right = right
+        self.op = op
+
+class VarCreateNode(Node):
+    def __init__(self, name: str, value: Node):
+        super().__init__(NodeType.VarCreateNode, 0)
+        self.name = name
+        self.value = value
+
+class VarAccessNode(Node):
+    def __init__(self, name: str):
+        super().__init__(NodeType.VarAccessNode, 0)
+        self.name = name
+
+class VarOverrideNode(Node):
+    def __init__(self, name: str, value: Node):
+        super().__init__(NodeType.VarOverrideNode, 0)
+        self.name = name
+        self.value = value
+
+class ArrayNode(Node):
+    def __init__(self, value: dict):
+        super().__init__(NodeType.ArrayNode, 0)
+        self.value = value
+
+class ObjectNode(Node):
+    def __init__(self, value: dict):
+        super().__init__(NodeType.ObjectNode, 0)
+        self.value = value
+
+class DataStructOverrideNode(Node):
+    def __init__(self, name: str, path: list, val: Node):
+        super().__init__(NodeType.DataStructOverrideNode, 0)
+        self.name = name
+        self.path = path
+        self.value = val
+
+class IndexNode(Node):
+    def __init__(self, name: str, path: list):
+        super().__init__(NodeType.IndexNode, 0)
+        self.name = name
+        self.path = path
+
+class ReturnNode(Node):
+    def __init__(self, returnValue: Node = Node(NodeType.NullNode, 0)):
+        super().__init__(NodeType.ReturnNode, 0)
+        self.returnValue = returnValue
+
+class FunctionNode(Node):
+    def __init__(self, args: list, block: list):
+        super().__init__(NodeType.FunctionNode, 0)
+        self.args = args
+        self.block = block
+
+class FunctionCallNode(Node):
+    def __init__(self, args: list, name: str, path: IndexNode = None, hasPath : bool = False):
+        super().__init__(NodeType.FunctionCallNode, 0)
+        self.args = args
+        self.name = name
+        self.path = path
+        self.haspath = hasPath
+
+class IfStatementNode(Node):
+    def __init__(self, condition: CondNode, body: list = [], elifs: list = [], _else: Node = None):
+        super().__init__(NodeType.IfStatementNode, 0)
+        self.condition = condition
+        self.body = body
+        self.elifs = elifs
+        self._else = _else
+
+class BaseGlobalLog(Node):
+    def __init__(self, message: Node):
+        super().__init__(NodeType.BaseGlobalLog, 0)
+        self.message = message
+
+class BaseGlobalSleep(Node):
+    def __init__(self, number: Node):
+        super().__init__(NodeType.BaseGlobalSleep, 0)
+        self.number = number
